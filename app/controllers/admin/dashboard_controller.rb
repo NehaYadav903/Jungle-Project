74bfe14b377a4914c64891e07a61bec29eff324b
Neha Yadav <nehayadav903@gmail.com>
2022-01-16 23:31:00 -0500

    Fix the file name

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["J_USERNAME"], password: ENV["J_PASSWORD"]
 
   def show
+    @category = Category.all
+    @product = Product.all
   end
 end
